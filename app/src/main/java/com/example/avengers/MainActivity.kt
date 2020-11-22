ec82ddf4521d1a25d59fb0395cbfb88fa22eca2a
Andrew Martynov <martynov.andrew14@gmail.com>
2020-11-23 00:39:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/avengers/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.avengers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
